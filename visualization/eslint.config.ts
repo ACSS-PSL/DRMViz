a89983ba5321a196e17b118a7901d505d1496f5f
WilliamAboucaya <william.aboucaya@dauphine.psl.eu>
2025-11-25 14:05:57 +0100

    Add project config files for visualization

---
+++ visualization/eslint.config.ts
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginTSeslint from "@typescript-eslint/eslint-plugin";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+import reactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: ["build", "dist", "node_modules"],
+  },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  reactHooks.configs.flat.recommended,
+  { 
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...pluginTSeslint.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react/require-default-props": "off",
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' }
+      ]
+    }
+  },
+]);
